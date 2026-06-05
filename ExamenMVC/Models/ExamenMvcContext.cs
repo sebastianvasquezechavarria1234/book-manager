1c85c515921148a7815ebde30f760541a40643c2
sebastianvasquezechavarria1234 <sebasvasquez1314@gmail.com>
2026-06-05 12:22:02 -0500

    fix: remove hardcoded connection strings from DbContext files
    Both ExamenMvcContext and CrudSebastianVasquezContext contained
    hardcoded SQL Server connection strings in OnConfiguring() methods.
    These are a security risk and were also flagged by compiler warnings.
    Connection strings are now managed exclusively through appsettings.json
    via dependency injection.

--- ExamenMVC/Models/ExamenMvcContext.cs
+++ ExamenMVC/Models/ExamenMvcContext.cs
@@ -19,10 +19,6 @@ public partial class ExamenMvcContext : DbContext
 
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=DESKTOP-OSQK0RQ;Initial Catalog=ExamenMVC;integrated security=True;TrustServerCertificate=True");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Animale>(entity =>
